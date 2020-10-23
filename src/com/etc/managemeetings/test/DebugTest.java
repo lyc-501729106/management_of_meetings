929723747387ccf592ea17b28b00972127e4af59
lyc <501729106@qq.com>
2020-10-23 18:06:57 +0800

    写了个DebugTest.java类

---
+++ src/com/etc/managemeetings/test/DebugTest.java
@@ -0,0 +1,16 @@
+package com.etc.managemeetings.test;
+
+import java.util.HashMap;
+
+/**
+ * @author 吕译辰
+ * @date 2020/10/23 - 17:39
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        HashMap<String , String> map = new HashMap<>();
+        map.put("name","tom");
+        map.put("age","12");
+        map.put("school","Tsinghua");
+    }
+}
